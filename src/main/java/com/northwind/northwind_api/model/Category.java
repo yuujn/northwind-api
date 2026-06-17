e2c4a2b8b87b492155a5f068f2a1f8c3f00d519b
yuujn <pale.cup5223@fastmail.com>
2026-06-17 10:41:05 -0500

    do task 1 from https://github.com/craigmckeachie/notes-yu-spring-2026/blob/main/spring/3-jpa-relationships-with-mini-lab.md#7-exercise

---
+++ src/main/java/com/northwind/northwind_api/model/Category.java
@@ -0,0 +1,39 @@
+package com.northwind.northwind_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CategoryID")
+    private Long id;
+
+    private String categoryName;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
