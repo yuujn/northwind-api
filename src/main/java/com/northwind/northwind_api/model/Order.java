ac9bcb3381cc78aa22b89cf5575a641a4d71d52c
yuujn <pale.cup5223@fastmail.com>
2026-06-17 13:33:38 -0500

    do task 1 from https://github.com/craigmckeachie/notes-yu-spring-2026/blob/main/spring/4-northwind-orders-lab.md

---
+++ src/main/java/com/northwind/northwind_api/model/Order.java
@@ -0,0 +1,141 @@
+package com.northwind.northwind_api.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "OrderID")
+    private Long id;
+
+    private Long customerId;
+    private Long employeeId;
+    private LocalDate orderDate;
+    private LocalDate requiredDate;
+    private LocalDate shippedDate;
+    private Long shipVia;
+    private BigDecimal freight;
+    private String shipName;
+    private String shipAddress;
+    private String shipCity;
+    private String shipRegion;
+    private String shipPostalCode;
+    private String shipCountry;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(LocalDate requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public LocalDate getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(LocalDate shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public Long getShipVia() {
+        return shipVia;
+    }
+
+    public void setShipVia(Long shipVia) {
+        this.shipVia = shipVia;
+    }
+
+    public BigDecimal getFreight() {
+        return freight;
+    }
+
+    public void setFreight(BigDecimal freight) {
+        this.freight = freight;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getShipAddress() {
+        return shipAddress;
+    }
+
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+
+    public String getShipCity() {
+        return shipCity;
+    }
+
+    public void setShipCity(String shipCity) {
+        this.shipCity = shipCity;
+    }
+
+    public String getShipRegion() {
+        return shipRegion;
+    }
+
+    public void setShipRegion(String shipRegion) {
+        this.shipRegion = shipRegion;
+    }
+
+    public String getShipPostalCode() {
+        return shipPostalCode;
+    }
+
+    public void setShipPostalCode(String shipPostalCode) {
+        this.shipPostalCode = shipPostalCode;
+    }
+
+    public String getShipCountry() {
+        return shipCountry;
+    }
+
+    public void setShipCountry(String shipCountry) {
+        this.shipCountry = shipCountry;
+    }
+}
