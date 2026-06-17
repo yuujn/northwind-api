4744bdf56acb1698a1d2382d074f7d1dac325a86
yuujn <pale.cup5223@fastmail.com>
2026-06-17 10:47:52 -0500

    do task 3

--- src/main/java/com/northwind/northwind_api/repository/ProductRepository.java
+++ src/main/java/com/northwind/northwind_api/repository/ProductRepository.java
@@ -2,12 +2,15 @@ package com.northwind.northwind_api.repository;
 
 import com.northwind.northwind_api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.category")
+    List<Product> findAllWithCategories();
     List<Product> findByCategoryId(Long categoryId);
     List<Product> findByProductNameContainingIgnoreCase(String name);
 }
